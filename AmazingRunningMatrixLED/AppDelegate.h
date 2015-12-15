fa51880145c5b93446f4d97ee6a4ac3291478f66
IM Hoang Thai <hoangthai95ne@gmail.com>
2015-12-15 21:28:49 +0700

    Initial Commit

---
+++ AmazingRunningMatrixLED/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AmazingRunningMatrixLED
+//
+//  Created by Hoàng Thái on 12/15/15.
+//  Copyright © 2015 HOANGTHAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AmazingRunningMatrixLED/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AmazingRunningMatrixLED
+//
+//  Created by Hoàng Thái on 12/15/15.
+//  Copyright © 2015 HOANGTHAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
